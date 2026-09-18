9cd05c382eae47fe8d41f7ce6ea784e6495784a5
T04-ka <sushkov.infor.548@yandex.ru>
2026-09-18 14:32:04 +0300

    Сделал аккуратный qsort

--- Onegin/qsort.cpp
+++ Onegin/qsort.cpp
@@ -1,24 +1,29 @@
-/*
-#ifndef QSORT_CPP
-#define QSORT_CPP
 #include <stdio.h>
 #include <assert.h>
-#endif
-*/
+#include <string.h>
+
+enum Cmp    {
+    LESS = -1,
+    EQ = 0,
+    MORE = 1
+};
+
+
 
 //----------------------------------------------------------------------------------------------------------------
 /// Sorting the void* array with personal comparator given
 ///
 /// @param[in] arr[]     Array with data
-/// @param[in] leftbrdr  Left border of array part to sort
-/// @param[in] rightbrdr Right border of array part to sort
+/// @param[in] elemsize  Size of one array element in bytes
+/// @param[in] arrsize   Size of array in bytes
 /// @param[in] cmp       Personal comparator
 ///
 /// @note Comparator must return 0 if elements are equal,
 ///                              positive int if first elem > second elem,
 ///                              negative int if first elem < second elem.
 //----------------------------------------------------------------------------------------------------------------
-void qsort(void* arr[], size_t leftbrdr, size_t rightbrdr, int (*cmp)(void *a, void *b));
+void qsort(void* arr, size_t elemsize, size_t arrsize, int (*cmp)(void *a, void *b));
+
 
 
 //----------------------------------------------------------------------------------------------------------------
@@ -26,84 +31,63 @@ void qsort(void* arr[], size_t leftbrdr, size_t rightbrdr, int (*cmp)(void *a, v
 ///
 /// @param[in] i    Pointer to first element
 /// @param[in] j    Pointer to second element
+/// @param[in] size Size of elements to swap
 ///
 //----------------------------------------------------------------------------------------------------------------
-void swap(void* *i, void* *j);
+void swap(void* e1ptr, void* e2ptr, size_t size);
 
 
 
 //----------------------------------------------------------------------------------------------------------------
-void qsort(void* arr[], size_t leftbrdr, size_t rightbrdr, int (*cmp)(void *a, void *b)){
+void qsort(void* arr, size_t elemsize, size_t arrsize, int (*cmp)(void *e1ptr, void *e2ptr)){
 
-          //  assert(arr != NULL);
+    size_t leftbrdr = 0;
+    size_t rightbrdr = arrsize/elemsize - 1;
 
-            if (leftbrdr >= rightbrdr){
+    if (leftbrdr >= rightbrdr || arrsize <= 0){
 
-                return;
-            }
-            /*
-            assert(leftbrdr < rightbrdr);
-            assert(leftbrdr >= 0);
-            */
-            void* midval = arr[(leftbrdr + rightbrdr)/2];
+        return;
+    }
 
-            size_t leftptr = leftbrdr;
-            size_t rightptr = rightbrdr;
+    size_t leftptr = leftbrdr;
+    size_t rightptr = rightbrdr;
 
-            //pdbg("Start qsort\n");
+    size_t adress = elemsize * ((leftbrdr + rightbrdr)/2);
 
-            while (leftptr < rightptr){
+    void* pivot = (void*) ((char*) arr + adress);
 
-                //pdbg("Begin of cicl\n");
-                /*
-                assert(rightptr >= leftbrdr);
-                assert(leftptr <= rightbrdr);
-                assert(rightptr <= rightbrdr);
-                assert(leftptr >= leftbrdr);
-                */
-                if ((*cmp)(arr[leftptr], midval) >= 0) {
+    while (leftptr < rightptr){
 
-                    while (rightptr > leftptr && (*cmp)(arr[rightptr], midval) > 0)
-                    {
-                        --rightptr;
-                        /*
-                        assert(rightptr >= leftbrdr);
-                        assert(leftptr <= rightbrdr);
-                        assert(rightptr <= rightbrdr);
-                        assert(leftptr >= leftbrdr);
-                        */
-                        //pdbg("Rfind cicl\n");
-                    }
+        if ((*cmp)((void*) ((char*) arr + elemsize * leftptr), pivot) >= 0) {
 
-                    //pdbg("BEFORESWAP\n");
-
-                    //printf("RPTR = %lu\n", rightptr);
-                    swap(arr + leftptr, arr + rightptr);
-
-                    //pdbg("AFTERSWAP\n");
-
-                }
-
-                leftptr++;
+            while (rightptr > leftptr && (*cmp)((void*) ((char*) arr + elemsize * rightptr), pivot) > 0)
+            {
+                --rightptr;
             }
 
-            //pdbg("After cicl\n");
+            swap((void*) ((char*) arr + elemsize * leftptr), (void*) ((char*) arr + elemsize * rightptr), elemsize);
+        }
 
-            qsort(arr, leftbrdr, leftptr - 1, cmp);
-            qsort(arr, rightptr + 1, rightbrdr, cmp);
-}
+        leftptr++;
+    }
 
-
-
-//----------------------------------------------------------------------------------------------------------------
-void swap(void* *i, void* *j){
-//void swap(void **i, void **j){
-
-    void* temp = *i;
-    *i = *j;
-    *j = temp;
+    qsort(arr, elemsize, elemsize * (leftptr - leftbrdr), cmp);
+    qsort((void*) ((char*) arr + elemsize * (rightptr + 1)), elemsize, elemsize * (rightbrdr - rightptr), cmp);
 }
 
 
 //----------------------------------------------------------------------------------------------------------------
+void swap(void* e1ptr, void* e2ptr, size_t size){
 
+    for (size_t i = 0; i < size; i++){
+
+        char tmpbyte = *((char *) e1ptr + i);
+
+        *((char *) e1ptr + i) = *((char *) e2ptr + i);
+
+        *((char *) e2ptr + i) = tmpbyte;
+    }
+}
+
+
+//----------------------------------------------------------------------------------------------------------------
